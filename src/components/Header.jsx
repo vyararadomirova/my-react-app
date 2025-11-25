40771e6c14cde7917542e856932bf5051742303c
Vyara Radeva <vyara.radomirova@gmail.com>
2025-11-25 22:03:20 +0200

    Style Header and finalize layout for initial page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/" >Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </nav>
+ <header className={styles.header}>
+     <nav className={styles.nav}>
+             <div className={styles.logo}>Пътеводителят на Верчето</div>
+     
+             <ul>
+               <li>
+                 <Link to="/">Начало</Link>
+               </li>
+     
+               <li>
+                 <Link to="/register">Регистрация</Link>
+               </li>
+     
+               <li>
+                 <Link to="/login">Вход</Link>
+               </li>
+             </ul>
+           </nav>
+
+      <div className={styles.hero}>
+        <h1>Открий красотата на родината</h1>
+        <p>Вдъхновявай се. Пътувай. Обичай България.</p>
+      </div>
     </header>
   );
 }
-
