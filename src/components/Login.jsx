6effc099f2ffd333e637e868d0cffa1e2244f46c
Vyara Radeva <vyara.radomirova@gmail.com>
2025-11-25 22:59:37 +0200

    Add styled Login and Register forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './Form.module.css';
 
-export default function Home() {
+export default function Login() {
     return (
-          <h1>Login</h1>
+          <main>
+            <div className={styles.formContainer}>
+                <h1>Вход</h1>
+                <form className={styles.form}>
+                    <label htmlFor="name">Име</label>
+                    <input type="text" id="name" name="name" required />
+
+                    <label htmlFor="email">Имейл</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <label htmlFor="password">Парола</label>
+                    <input type="password" id="password" name="password" required />
+
+                    <button type="submit" className={styles.formBtn}>
+                        Вход
+                    </button>
+                </form>
+            </div>
+        </main>
     );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styles from './Form.module.css';
 
-export default function Home() {
+export default function Register() {
     return (
-          <h1>Register</h1>
+        <main>
+            <div className={styles.formContainer}>
+                <h1>Регистрация</h1>
+                <form className={styles.form}>
+                    <label htmlFor="name">Име</label>
+                    <input type="text" id="name" name="name" required />
+
+                    <label htmlFor="email">Имейл</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <label htmlFor="password">Парола</label>
+                    <input type="password" id="password" name="password" required />
+
+                    <label htmlFor="re-password">Повторете парола</label>
+                    <input
+                        type="password"
+                        id="re-password"
+                        name="rePassword"
+                        required
+                    />
+
+                    <button type="submit" className={styles.formBtn}>
+                        Регистрирай се
+                    </button>
+                </form>
+            </div>
+        </main>
     );
 }
